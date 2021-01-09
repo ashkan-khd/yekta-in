66dd90141f2614f6ff69b75b10ba01463bfd787c
alikhd1 <alikhandi@gmail.com>
2021-01-09 23:48:53 +0330

    add ashkan test

---
+++ test.py
@@ -0,0 +1,34 @@
+from ad import Ad
+from advertiser import Advertiser
+
+
+adver1 = Advertiser('ash')
+adver2 = Advertiser('ali')
+
+ad1 = Ad('spring', 'link', 'link', adver1)
+ad2 = Ad('winter', 'link', 'link', adver1)
+ad3 = Ad('winter', 'link', 'link', adver2)
+ad1.incClicks()
+ad1.incClicks()
+ad1.incClicks()
+
+ad2.incClicks()
+ad2.incClicks()
+
+print('Clicks: should be 3',ad1.clicks)
+print('Clicks: should be 2',ad2.clicks)
+print('Clicks: should be 0',ad3.clicks)
+
+
+
+
+print('total Clicks',Advertiser.getTotalClicks())
+
+
+
+# ad  nm   co
+# ad1 ash  3
+# ad2 ash  2
+# ash      5
+# ad3 ali  0
+
